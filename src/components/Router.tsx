6a3a3aca824e88eea04ccefd6c5d371d98d01ec0
Kevin Niederelz <kevin.niederelz@student.syntax-institut.de>
2026-06-03 17:40:14 +0200

    removed unnecessary pages

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 
 import DogsOverview from "./pages/main/DogsOverview"
-import DogAppointment from "./pages/main/DogAppointment"
-import UserFavorites from "./pages/main/UserFavorites"
 import UserAppointments from "./pages/main/UserAppointments"
 import UserProfile from "./pages/main/UserProfile"
 
@@ -10,8 +8,6 @@ export default function Router() {
     return (
         <Routes>
             <Route path="*" element={<DogsOverview />} />
-            <Route path="dogAppointment/:dogId" element={<DogAppointment />} />
-            <Route path="userFavorites" element={<UserFavorites />} />
             <Route path="userAppointments" element={<UserAppointments />} />
             <Route path="userProfile" element={<UserProfile />} />
         </Routes>
--- src/components/pages/main/DogAppointment.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-export default function DogAppointment() {
-  return (
-    <>
-      <Heading>Dogs Appointment</Heading>
-    </>
-  )
-}
