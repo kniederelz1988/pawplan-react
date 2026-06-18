98d3ee78dd6e22a73d0ee6658407eb8a36577f67
Kevin Niederelz <kevin.niederelz@student.syntax-institut.de>
2026-06-18 15:30:38 +0200

    reworked the design of the user profile page

--- src/components/pages/main/UserProfile.tsx
+++ src/components/pages/main/UserProfile.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 
-import { Box, Circle, Flex, Grid, GridItem, Heading, HStack, IconButton, Link, Text, VStack } from "@chakra-ui/react";
+import { AspectRatio, Box, Circle, Container, Flex, Grid, GridItem, Heading, HStack, IconButton, Link, Spacer, Text, VStack } from "@chakra-ui/react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FiLogIn } from "react-icons/fi";
 import { BiPencil } from "react-icons/bi";
@@ -42,47 +42,50 @@ export default function UserProfile() {
                     Your volunteer account and activity.
                 </Text>
                 
-                <Box bgColor={"primary"} border={"xs"} borderRadius={"3xl"} borderColor={"softAccent.fg"} boxShadow={"md"} mt={8} p={6}>
-                    <HStack>
-                        <Circle w="64px" h="64px" fontSize={"3xl"} bgColor="accent.bg" color="accent.fg">
+                <Box bgColor={"primary"} border={"xs"} borderRadius={"3xl"} borderColor={"softAccent.fg"} boxShadow={"md"} mt={8} p={0}>
+                    <HStack gap={4} p={4} align={"stretch"}>
+                        <Circle w="80px" h="80px" fontSize={"3xl"} bgColor="accent.bg" color="accent.fg">
                             {volunteer.name.substring(0, 1).toUpperCase()}
                         </Circle>
 
-                        <VStack gap={0} pl={2} w="100%">
+                        <VStack m={"auto"} gap={0}>
                             <Text fontSize="md" fontWeight="bold" w="100%">
                                 {volunteer.name}
-                            </Text>
-                            <Text fontSize="sm" w="100%">{user.email}</Text>
-                            <Flex w="100%" pt={1}>
                                 <ProfileBadge role={role} />
-                            </Flex> 
+                            </Text>
+                            <Text fontSize="sm" w="100%">
+                                {user.email}
+                            </Text>
                         </VStack>
 
-                        <IconButton variant="subtle" borderRadius={24} onClick={onEditUserClick}>
-                            <BiPencil />
-                        </IconButton>
+                        <Spacer />
+                        <Spacer />
+
+                        <Grid templateColumns={"repeat(2, 1fr)"} gap={2} p={0} display={{ base: "none", md: "grid"}}>
+                            <GridItem colSpan={1} bgColor={"softAccent.bg/10"} color={"softAccent.fg"} borderRadius={"2xl"} border={"xs"} boxShadow={"sm"}>
+                                <VStack align={"center"} gap={0} p={4}>
+                                    <Text fontWeight={"bold"}>{appointments.length}</Text>
+                                    <Text>visits</Text>
+                                </VStack>
+                            </GridItem>
+
+                            <GridItem colSpan={1} bgColor={"softAccent.bg/10"} color={"softAccent.fg"} borderRadius={"2xl"} border={"xs"} boxShadow={"sm"}>
+                                <VStack align={"center"} gap={0} p={4}>
+                                    <Text fontWeight={"bold"}>{likeCounter}</Text>
+                                    <Text>favourites</Text>
+                                </VStack>
+                            </GridItem>
+                        </Grid>
+                        
+                        <Spacer />
+
+                        <VStack m={"auto"} gap={0}>
+                            <IconButton variant="subtle" borderRadius={24} onClick={onEditUserClick}>
+                                <BiPencil />
+                            </IconButton>
+                        </VStack>
                     </HStack>
                 </Box>
-
-                <Grid templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(4, 1fr)" }} gap={4} mt={8}>
-                    <GridItem colSpan={{ md: 1 }} display={{ base: "none", md: "block"}}></GridItem>
-
-                    <GridItem colSpan={1} justifyContent="center" bgColor={"softAccent.bg/10"} color={"softAccent.fg"} borderRadius={"2xl"} border={"xs"} boxShadow={"sm"} p={4}>
-                        <VStack gap={0}>
-                            <Text fontWeight={"bold"}>{appointments.length}</Text>
-                            <Text>total visits</Text>
-                        </VStack>
-                    </GridItem>
-
-                    <GridItem colSpan={1} justifyContent="center" bgColor={"softAccent.bg/10"} color={"softAccent.fg"} borderRadius={"2xl"} border={"xs"} boxShadow={"sm"} p={4}>
-                        <VStack gap={0}>
-                            <Text fontWeight={"bold"}>{likeCounter}</Text>
-                            <Text>favourites</Text>
-                        </VStack>
-                    </GridItem>
-
-                    <GridItem colSpan={{ base: 0, md: 1 }}></GridItem>
-                </Grid>
 
                 <Box bgColor={"softAccent.bg/30"} color={"softAccent.fg"} borderRadius={"3xl"} boxShadow={"md"} mt={8} p={6}>
                     <HStack alignItems="center">
